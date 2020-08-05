73ba4fb68c0ea48a4e6d31ec85109cc4bb399569
VasilyGorbunov <vasily.gorbunov@gmail.com>
2020-08-05 21:58:41 +0300

    4-2

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const logger = require('./middleware/logger')
+const connectDB = require('./config/db')
 
 // Load env vars
 dotenv.config({path: './config/config.env'})
+
+// Connect to database
+connectDB()
+
 // Load routes
 const bootcamps = require('./routes/bootcamps')
 
@@ -17,4 +22,11 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in mode ${process.env.NODE_ENV} on port ${PORT}`))
+const server = app.listen(PORT, console.log(`Server running in mode ${process.env.NODE_ENV} on port ${PORT}`))
+
+// Handle unhadled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`)
+  // Close server & exit process
+  server.close(() => process.exit(1))
+})
